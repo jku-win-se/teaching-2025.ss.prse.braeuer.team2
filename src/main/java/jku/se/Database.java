d4693d4a7cb3aa2bfe427a8b62023c49494e6be8
Lukas Mayr <lukasmayr83@gmail.com>
2025-03-22 12:39:39 +0100

    update

--- src/main/java/jku/se/Database.java
+++ src/main/java/jku/se/Database.java
@@ -6,20 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Database {
-    public static void main(String[] args) {
-        String url = "jdbc:postgresql://aws-0-eu-central-1.pooler.supabase.com:6543/postgres";
-        String user = "postgres.pwltfjlqripcrhenhgnk";
-        String password = "ujCpo7WdTPUzWpss"; // Replace with your actual password
 
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connected to the database!");
-
-            insertRechnung(connection, "User1",19.99, "2024-03-19", invoice_typ.Restaurant, false);
-            // Perform database operations here
-        } catch (SQLException e) {
-            System.out.println("Connection failed: " + e.getMessage());
-        }
-    }
 
     public static void insertRechnung(Connection connection, String username, double betrag, String datum, invoice_typ typ,boolean proved) {
         String sql = "INSERT INTO rechnungen (username,betrag, datum, typ,proved) VALUES (?, ?, ?,?,?)";
--- src/main/java/jku/se/Main.java
+++ src/main/java/jku/se/Main.java
@@ -4,12 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Main {
-    private static final String URL = "jdbc:postgresql://postgres:[YOUR-PASSWORD]@db.pwltfjlqripcrhenhgnk.supabase.co:5432/postgres";
-    private static final String USER = "Lunchify";
-    private static final String PASSWORD = "ujCpo7WdTPUzWpss";
+import static jku.se.Database.insertRechnung;
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://aws-0-eu-central-1.pooler.supabase.com:6543/postgres";
+        String user = "postgres.pwltfjlqripcrhenhgnk";
+        String password = "ujCpo7WdTPUzWpss"; // Replace with your actual password
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connected to the database!");
+
+            insertRechnung(connection, "User1",19.99, "2024-03-19", invoice_typ.Restaurant, false);
+            // Perform database operations here
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
     }
 }
