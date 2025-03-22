8b21f4bbdf766c8b580a1894fa2cd3910eece4da
lukasmayr83 <lukasmayr83@gmail.com>
2025-03-22 11:27:05 +0100

    Add files via upload

---
+++ src/main/java/jku/se/Main.java
@@ -0,0 +1,15 @@
+package jku.se;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    private static final String URL = "jdbc:postgresql://postgres:[YOUR-PASSWORD]@db.pwltfjlqripcrhenhgnk.supabase.co:5432/postgres";
+    private static final String USER = "Lunchify";
+    private static final String PASSWORD = "ujCpo7WdTPUzWpss";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
