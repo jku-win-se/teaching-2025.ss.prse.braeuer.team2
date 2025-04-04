9bdac690c5a7b665207935227453a797f75ae4ed
lukasmayr83 <lukasmayr83@gmail.com>
2025-04-04 18:29:36 +0200

    Update DashboardUserController.java

--- src/main/java/jku/se/Controller/DashboardUserController.java
+++ src/main/java/jku/se/Controller/DashboardUserController.java
@@ -1,38 +1,19 @@
 package jku.se.Controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import jku.se.Controller.SubmitBillController;
-
 
 public class DashboardUserController extends Controller {
 
     @FXML
-    private DashboardUserController dashboardUserController;
+    private void openSubmitBill(ActionEvent event) throws IOException {
+        switchScene(event, "submitBill.fxml");
+    }
 
     @FXML
     private void openSubmittedBills(ActionEvent event) throws IOException {
         switchScene(event, "submittedBills.fxml");
     }
-
-
-    public void setDashboardUserController(DashboardUserController controller) {
-        this.dashboardUserController = controller;
-    }
-
 
 }
