81606137a7a42c7e104e9223633786a55d560a31
Florian1910 <florian1.aigner@gmail.com>
2025-04-24 15:18:24 +0200

    code überarbeiten

--- src/main/java/jku/se/Controller/AddUserController.java
+++ src/main/java/jku/se/Controller/AddUserController.java
@@ -3,14 +3,12 @@ package jku.se.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
-import javafx.scene.Node;
 import jku.se.UserManagement;
 import jku.se.Role;
 
 import java.io.IOException;
 import java.sql.SQLException;
 
-import static jku.se.Controller.RequestManagementController.showAlert;
 
 public class AddUserController extends Controller {
 
